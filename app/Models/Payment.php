ee8019ab5300cb7d88e8cd8274d1390b301bedfb
unilley <122447945+allyeahh@users.noreply.github.com>
2025-08-30 10:53:57 +0800

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -10,19 +10,19 @@ class Payment extends Model
     use HasFactory;
 
     protected $table = 'payment';
-    protected $primaryKey = 'payment_id'; 
-    public $timestamps = false; 
+    protected $primaryKey = 'payment_id';
+    public $timestamps = false;
 
     protected $fillable = [
         'owner_id',
-        'payment_mode',         
-        'payment_acc_number',  
-        'payment_amount',       
-        'payment_date',         
+        'payment_mode',
+        'payment_acc_number',
+        'payment_amount',
+        'payment_date',
     ];
 
     public function owner()
     {
-        return $this->belongsTo(Owner::class, 'owner_id', 'owner_id'); 
+        return $this->belongsTo(Owner::class, 'owner_id', 'owner_id');
     }
 }
